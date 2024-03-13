4de664379a10523d56fb906a64f07292e2aa17eb
Hugo Perdomo <hugo@perdomo.dev>
2024-03-13 10:54:21 -0400

    adds tmux navigator + Aura color + lsp for markdown

--- lua/base/lazy.lua
+++ lua/base/lazy.lua
@@ -1,83 +1,101 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 return require("lazy").setup({
-  {
-      "nvim-telescope/telescope.nvim", tag = "0.1.5",
-	  -- or                            , branch = "0.1.x",
-	  dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {
-	  "rose-pine/neovim",
-	  name = "rose-pine",
-	  config = function()
-		  vim.cmd("colorscheme rose-pine")
-	  end
-  },
-  {
-      "folke/trouble.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons"  },
-      opts = {
-        -- icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-  },
-  {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-  },
-  {
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end
-  },
-  "nvim-treesitter/playground",
-  "theprimeagen/harpoon",
-  "theprimeagen/refactoring.nvim",
-  "mbbill/undotree",
-  "tpope/vim-fugitive",
-  "nvim-treesitter/nvim-treesitter-context",
-  {
-	  "VonHeikemen/lsp-zero.nvim",
-	  branch = "v3.x",
-	  dependencies = {
-		  -- LSP Support
-		  {"neovim/nvim-lspconfig"},
-		  {"williamboman/mason.nvim"},
-		  {"williamboman/mason-lspconfig.nvim"},
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        -- or                            , branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons"  },
+        opts = {
+            -- icons = false,
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    {
+        "baliestri/aura-theme",
+        lazy = false,
+        priority = 1000,
+        config = function(plugin)
+            vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+            vim.cmd([[colorscheme aura-dark]])
+        end
+    },
+    "nvim-treesitter/playground",
+    "theprimeagen/harpoon",
+    "theprimeagen/refactoring.nvim",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "nvim-treesitter/nvim-treesitter-context",
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        dependencies = {
+            -- LSP Support
+            {"neovim/nvim-lspconfig"},
+            {"williamboman/mason.nvim"},
+            {"williamboman/mason-lspconfig.nvim"},
 
-		  -- Autocompletion
-		  {"hrsh7th/nvim-cmp"},
-		  {"hrsh7th/cmp-buffer"},
-		  {"hrsh7th/cmp-path"},
-		  {"saadparwaiz1/cmp_luasnip"},
-		  {"hrsh7th/cmp-nvim-lsp"},
-		  {"hrsh7th/cmp-nvim-lua"},
+            -- Autocompletion
+            {"hrsh7th/nvim-cmp"},
+            {"hrsh7th/cmp-buffer"},
+            {"hrsh7th/cmp-path"},
+            {"saadparwaiz1/cmp_luasnip"},
+            {"hrsh7th/cmp-nvim-lsp"},
+            {"hrsh7th/cmp-nvim-lua"},
 
-		  -- Snippets
-		  {"L3MON4D3/LuaSnip"},
-		  {"rafamadriz/friendly-snippets"},
-	  }
-  },
-  "folke/zen-mode.nvim",
-  "github/copilot.vim",
-  "eandrju/cellular-automaton.nvim",
-  "laytan/cloak.nvim",
+            -- Snippets
+            {"L3MON4D3/LuaSnip"},
+            {"rafamadriz/friendly-snippets"},
+        }
+    },
+    "folke/zen-mode.nvim",
+    "github/copilot.vim",
+    "eandrju/cellular-automaton.nvim",
+    "laytan/cloak.nvim",
+    { 
+        "christoomey/vim-tmux-navigator", 
+        lazy = false,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
 })
 
